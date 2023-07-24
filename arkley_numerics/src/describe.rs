1fb66443945b7be5f06b230deacad14de4bcf901
Deaths-Door <aaravaditya51@gmail.com>
2023-07-24 18:51:43 +0200

    done for now

--- arkley_numerics/src/describe.rs
+++ arkley_numerics/src/describe.rs
@@ -1,17 +1,36 @@
-use arkley_describe::Describe;
+use arkley_describe::{Describe,DescribeOperation,FilterLevel,Step};
+use arkley_traits::ArithmeticCore;
 
-impl Describe for Fraction<T> where T : ArithmeticCore {
+use super::{Fraction,Decimal};
+
+impl<T> Describe for Fraction<T> where T : ArithmeticCore {
     type Output = Step;
 
     fn describe(
         self,
-        other: $t,
+        _other: Self,
         filter_level: Option<FilterLevel>,
-        operation: DescribeOperation,
+        _operation: DescribeOperation,
     ) -> Option<Self::Output> {
         match filter_level.map(|level| level > FilterLevel::Advanced).unwrap_or(true) {
             false => None,
-            true => todo!("...")
+            true => todo!("not done yet")
+        }
+    }
+}
+
+impl Describe for Decimal {
+    type Output = Step;
+
+    fn describe(
+        self,
+        _other: Self,
+        filter_level: Option<FilterLevel>,
+        _operation: DescribeOperation,
+    ) -> Option<Self::Output> {
+        match filter_level.map(|level| level > FilterLevel::Intermediate).unwrap_or(true) {
+            false => None,
+            true => todo!("not done yet")
         }
     }
 }
